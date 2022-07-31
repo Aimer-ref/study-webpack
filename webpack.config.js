c9087f711fcdc9e2f39b2d6e829ed9d4b9c45b32
Aimer <leon-0606@foxmail.com>
2022-07-31 09:49:06 +0800

    chore: 添加webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,13 @@ const config = {
     filename: 'bundle.js'
   },
   mode: 'development',
+  devServer: {
+    open: true,
+    port: 8000,
+    // 不会每次都重新从内存中生成一份bundle.js 而是类似于打补丁的方式
+    hot: true,
+    // 本地服务器根路径 在html中路径 /开头 都是以这个目录为基准
+    static: path.resolve('./public')
+  }
 }
 module.exports = config
